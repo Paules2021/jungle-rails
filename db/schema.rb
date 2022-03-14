9565df66c4766373831f6f9a920020ba8399847f
Pouya Elishaee <p.elishaee@gmail.com>
2022-03-14 14:00:49 -0400

    install rspec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625062916) do
+ActiveRecord::Schema.define(version: 20220311124141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,9 @@ ActiveRecord::Schema.define(version: 20160625062916) do
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+  end
 
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
